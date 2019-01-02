a6a1dedaff2cc6630c8c0064506ad47e5c6df80e
Trent Moss <trentmoss05@gmail.com>
2019-01-02 05:33:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,23 @@
 def using_push(array, string)
   array.push(string)
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop
+
+end
+
+def pop_with_args
+
+end
+
+def using_shift
+
+end
+
+def shift_with_args
+
+end
